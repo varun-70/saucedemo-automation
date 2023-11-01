26067e83e3f55d393a0c5b868bf81e3d148461c3
Deep Verma <71069066+vermadeep10@users.noreply.github.com>
2023-11-01 15:43:32 +0530

    login page test cases

--- src/test/java/base/BaseClass.java
+++ src/test/java/base/BaseClass.java
@@ -1,36 +1,4 @@
 package base;
-//
-//import io.github.bonigarcia.wdm.WebDriverManager;
-//import org.openqa.selenium.WebDriver;
-//import org.openqa.selenium.firefox.FirefoxDriver;
-//import org.testng.annotations.AfterMethod;
-//import org.testng.annotations.BeforeMethod;
-//
-//import java.io.IOException;
-//import java.time.Duration;
-//
-//
-//public class BaseClass {
-//
-//    public static WebDriver driver;
-//    public String url;
-//
-//    @BeforeMethod
-//    public void setUp() throws IOException {
-//        WebDriverManager.firefoxdriver().setup();
-//        driver = new FirefoxDriver();
-//        driver.manage().window().maximize();
-//        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-//        url = "https://www.saucedemo.com/";
-//        driver.get(url);
-//
-//    }
-//
-//    @AfterMethod
-//    public void tearDown() {
-//        driver.quit();
-//    }
-//}
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
@@ -38,31 +6,30 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
+import java.io.IOException;
 import java.time.Duration;
 
-public class BaseClass {
-    private ThreadLocal<WebDriver> driverThreadLocal = new ThreadLocal<>();
 
-    protected WebDriver getDriver() {
-        return driverThreadLocal.get();
-    }
+public class BaseClass {
+
+    public static WebDriver driver;
+    public String url;
 
     @BeforeMethod
-    public void setUp() {
+    public void setUp() throws IOException {
         WebDriverManager.firefoxdriver().setup();
-        WebDriver driver = new FirefoxDriver();
-        driverThreadLocal.set(driver);
+        driver = new FirefoxDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-        driver.get("https://www.saucedemo.com/");
+        url = "https://www.saucedemo.com/";
+        driver.get(url);
+
     }
 
     @AfterMethod
     public void tearDown() {
-        WebDriver driver = getDriver();
-        if (driver != null) {
-            driver.quit();
-        }
-        driverThreadLocal.remove();
+        driver.quit();
     }
 }
+
+
